8171d48057ab15a0a870bdda02cfa3d3bf9bfc72
jinchao26664969 <26664969@qq.com>
2020-05-27 10:17:28 +0800

    分支添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -8,5 +8,6 @@ public static void satHelloGit()
 	System.out.println("hello git!");
 	System.out.println("hello git!更新1");
 	System.out.println("hello git!创建分支");
+	System.out.println("分支添加");
 }
 }
